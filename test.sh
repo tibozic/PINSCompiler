b7b20d2b4ecb43cad5ad0b063673b63a32e4b88a
tibozic <timotejbozic.tb@gmail.com>
2023-03-27 15:24:33 +0200

    Updated the test script.

--- test.sh
+++ test.sh
@@ -1,22 +1,24 @@
 
-TESTS="src/tests/parser/*.tst"
+TEST_PART="NAME"
+TESTS_PATH="src/tests/$TEST_PART"
+TEST_FILES="$TESTS_PATH/*.tst"
 
 echo "**** Testing ****"
-for testFile in $TESTS
+for testFile in $TEST_FILES
 do
 	# echo $testFile
 	echo "----- Testing file: $testFile -----"
 	testNum=${testFile//[^0-9]/}
-	java -classpath /home/betterjimmy/Documents/Projects/software/java/PINSCompiler/out/production/PINSCompiler:/home/betterjimmy/Documents/Projects/software/java/PINSCompiler/lib/ArgPar-0.1.jar Main PINS $testFile --dump LEX > "src/tests/test$testNum.new"
+	java -classpath /home/betterjimmy/Documents/Projects/software/java/PINSCompiler/out/production/PINSCompiler:/home/betterjimmy/Documents/Projects/software/java/PINSCompiler/lib/ArgPar-0.1.jar Main PINS $testFile --dump $TEST_PART --exec $TEST_PART &> "$TESTS_PATH/test$testNum.new"
 done
 
-TESTS="src/tests/*.out"
+TEST_RESULTS="$TESTS_PATH/*.out"
 
 echo "**** Results ****"
-for testFile in $TESTS
+for testFile in $TEST_RESULTS
 do
 	echo "----- Results of file: $testFile -----"
 	testNum=${testFile//[^0-9]/}
 	testNum=${testFile//[^0-9]/}
-	diff $testFile "src/tests/test$testNum.new"
+	diff $testFile "$TESTS_PATH/test$testNum.new"
 done
