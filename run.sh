6dc1ebbaa372f1b33ba0a7a23477170338575a96
tibozic <timotejbozic.tb@gmail.com>
2023-04-17 18:27:21 +0200

    Prepared for testing the frame creator.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 TEST_NUM="130"
-TEST_PART="TYP"
+TEST_PART="FRM"
 TEST_OUT="$TEST_PART"
 
 java -classpath /home/betterjimmy/Documents/Projects/software/java/PINSCompiler/out/production/PINSCompiler:/home/betterjimmy/Documents/Projects/software/java/PINSCompiler/lib/ArgPar-0.1.jar Main PINS src/tests/$TEST_PART/test$TEST_NUM.tst --dump $TEST_OUT --exec $TEST_OUT
